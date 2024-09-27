db1172554d89b5085ed74cec1569f416f4f2f23f
Lalit Patiil <113579777+Lalitpatill@users.noreply.github.com>
2024-09-27 22:11:41 +0530

    Update school.sql
    removed unwanted lines

--- school.sql
+++ school.sql
@@ -1,30 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 03, 2022 at 07:58 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `school`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -308,6 +288,4 @@ ALTER TABLE `teacher_details`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
